83ee0061c78e47f055be262de083448387a7cc40
Shogo Higashimoto <shogo.21.take@gmail.com>
2020-02-22 12:01:26 +0900

    index に初めてコミットしたファイル

---
+++ tutorial/resources/views/example.blade.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="ja">
+    {{--必要な情報の宣言--}}
+    <head>
+        <meta charset="utf-8">
+
+        <title>Example</title>
+        <!-- Bootstrap core CSS -->
+        <link href="/css/bootstrap.min.css" rel="stylesheet">
+    </head>
+
+    {{--本文--}}
+    <body>
+        <h1>Exampleページ</h1>
+        <div>
+            <table>
+                <thead>
+                    <tr>
+                        <th>タイトル</td>
+                        <th>カテゴリ</td>
+                        <th>作成者</td>
+                        <th>作成日</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($room_index as $room)
+                    <tr>
+                        <td><a href="">{{ $room['title'] }}</a></td>
+                        <td>{{ $room['category_name'] }}</td>
+                        <td>{{ $room['creator'] }}</td>
+                        <td>{{ $room['created_date'] }}</td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </body>
